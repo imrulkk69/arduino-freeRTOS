8dff75190feefb3d3c9f4893085b2778cdee031c
Imrul Khan <imrul.kais@brainstation23.com>
2021-06-10 13:34:31 +0600

    eventGroup with 3 task done

--- 11_event_groups/event_group_3_tasks/event_group_3_tasks.ino
+++ 11_event_groups/event_group_3_tasks/event_group_3_tasks.ino
@@ -1,10 +1,17 @@
-#include <avr8-stub.h>
-#include <app_api.h>
-
+#include <Arduino_FreeRTOS.h>
+#include "event_groups.h"
 
 #define RED 6
 #define YELLOW 7
 #define BLUE 8
+
+#define TASK1_BIT (1UL << 0UL)
+#define TASK2_BIT (1UL << 1UL)
+#define TASK3_BIT (1UL << 2UL)
+
+EventGroupHandle_t xEventGroup;
+
+const TickType_t xDelay500ms = pdMS_TO_TICKS(500);
 
 void setup()
 {
@@ -14,13 +21,70 @@ void setup()
   pinMode(RED, OUTPUT);
   pinMode(YELLOW, OUTPUT);
   pinMode(BLUE, OUTPUT);
+  
+  xEventGroup = xEventGroupCreate();
+  
+  xTaskCreate(vInputTask1, "Input Task 1", 100, NULL, 1, NULL);
+  xTaskCreate(vInputTask2, "Input Task 2", 100, NULL, 1, NULL);
+  xTaskCreate(vInputTask3, "Input Task 3", 100, NULL, 1, NULL);
+  xTaskCreate(vOutputTask, "Output Task", 100, NULL, 1, NULL);
+  
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  digitalWrite(BLUE, HIGH);
-  delay(500);
-  digitalWrite(BLUE, LOW);
-  delay(500);
+  // put your main code here, to run repeatedly
+}
+
+void vInputTask1(void *pvParameters)
+{
+	while(true)
+	{
+		xEventGroupSetBits(xEventGroup, TASK1_BIT);	
+		vTaskDelay(xDelay500ms);
+	}
+}
+
+void vInputTask2(void *pvParameters)
+{
+	while(true)
+	{
+		xEventGroupSetBits(xEventGroup, TASK2_BIT);
+		vTaskDelay(xDelay500ms);	
+	}
+}
+
+void vInputTask3(void *pvParameters)
+{
+	while(true)
+	{
+		xEventGroupSetBits(xEventGroup, TASK3_BIT);
+		vTaskDelay(xDelay500ms);	
+	}
+}
+
+void vOutputTask(void *pvParameters)
+{
+	const EventBits_t xBitsToWaitFor = (TASK1_BIT | TASK2_BIT | TASK3_BIT);
+	EventBits_t xEventGroupValue;
+	
+	while (true)
+	{
+		xEventGroupValue = xEventGroupWaitBits(xEventGroup, xBitsToWaitFor, pdTRUE, pdTRUE, portMAX_DELAY);
+		
+		if((xEventGroupValue & TASK1_BIT) !=0)
+		{
+			digitalWrite(RED, digitalRead(RED) ^ 1);
+		}
+		
+	    if((xEventGroupValue & TASK2_BIT) != 0)
+		{
+			digitalWrite(YELLOW, digitalRead(YELLOW) ^ 1);
+		}
+		
+	    if((xEventGroupValue & TASK3_BIT) != 0)
+		{
+			digitalWrite(BLUE, digitalRead(BLUE) ^ 1);
+		}	
+	}
 }
